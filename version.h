e31f39759c0f1c56c357a961c47c97ee0b319f31
Adem <swer.rock@gmail.com>
2020-05-09 19:57:00 +0300

    cleaning

--- version.h
+++ version.h
@@ -2,17 +2,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// TEST DENEME 
+
 
 #define MAJOR 1
 #define MINOR 0
 #define RELEASE 0
 #define BUILD 1568136
 
-#define LASTBUILD Sat May 09 14:28:47 2020
+#define LASTBUILD "Sat May 09 14:28:47 2020"
 
 
-//first filled up copy of the file is required otherwise corrupts the file, reason is known...
 
 
 
